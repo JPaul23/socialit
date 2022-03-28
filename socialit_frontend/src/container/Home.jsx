ac1c41311a1becfc33363dd216688d09e1261196
JPaul23 <73589069+JPaul23@users.noreply.github.com>
2022-03-28 22:03:33 +0200

    Auth-Login & Register Users

--- socialit_frontend/src/container/Home.jsx
+++ socialit_frontend/src/container/Home.jsx
@@ -1,8 +1,10 @@
-import React from 'react'
+import React from 'react';
 
 const Home = () => {
   return (
-    <div>Home</div>
+    <h1 className="text-3xl font-bold underline">
+      Hello world!
+    </h1>
   )
 }
 
